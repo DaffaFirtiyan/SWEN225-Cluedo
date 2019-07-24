f8cd8baadaab8a2551f16d1844574fa8d96ed201
Daffa Firtiyan <mdaffa.anzalna@gmail.com>
2019-07-25 11:18:10 +1200

    First Commit

---
+++ Cluedo/src/main/java/com/daffa/cluedo/Board.java
@@ -0,0 +1,15 @@
+package main.java.com.daffa.cluedo;
+
+public class Board {
+	public Board() {
+		init();
+	}
+	
+	public void init() {
+		return;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Cluedo!");
+	}
+}
